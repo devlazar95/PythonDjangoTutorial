f1178a006d0daa70942f86903cc0ad100f6632b6
Lazar Stankovic <lazarstankovic09@gmail.com>
2021-02-21 14:30:56 +0100

    Update settings.py

--- company/settings.py
+++ company/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'company',
         'USER': 'postgres',
-        'PASSWORD': 'dw1010rf',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }

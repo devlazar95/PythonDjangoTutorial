f07f960d1b9441501e6702b50bcdba00aaa77bdf
Dwarf95 <lakiwork95@gmail.com>
2021-02-22 20:06:00 +0100

    Adding views, serializers and url mapping

--- company/API/serializers.py
+++ company/API/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from company.API.models import Employee, Project, Sector
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +14,21 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class SectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sector
+        fields = '__all__'
--- company/urls.py
+++ company/urls.py
@@ -12,5 +12,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/', include('company.API.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
